54c53daeb032c809f77916fd94d74bcd6d1944e3
CryyZz <crisbrizuela94@gmail.com>
2020-02-19 02:20:46 -0300

    do while ejercicio 8

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,24 @@
 function mostrar()
 {
-
+	var num;
+	var prom;
 	var contador=0;
 	var acumulador=0;
 
+	while(contador <5)
+	{
+		num = parseInt(prompt("Ingrese un numero"));
 
+		acumulador = acumulador + num;
+
+		alert("La suma acumulada es " + acumulador);
+
+		contador++;
+	}
+
+	prom = acumulador / contador;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=prom;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,35 @@
 function mostrar()
 {
-
+	var num;
 	var contador=0;
 	var positivo=0;
+	var contPos=0;
 	var negativo=1;
+	var contNeg=0;
 	
-	var respuesta='si';
+	var respuesta;
 
+	do
+	{
+		num = parseInt(prompt("Ingrese un numero"));
+		if(num >= 0)
+		{
+			positivo = positivo + num;
+			document.getElementById('suma').value=positivo;
+			contPos++;
+		}
+		else
+		{
+			negativo = negativo * num;
+			document.getElementById('producto').value=negativo;
+			contNeg++;
+		}
+		respuesta = prompt("Quiere seguir ingresando numeros?");
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		contador++;
 
-}//FIN DE LA FUNCIÓN
+	}while(respuesta == 's');
+
+	alert("Se han ingresado " + contador + " numeros, " + contPos + " positivos y " + contNeg + " negativos");
+
+}
